efbb95a834e81590a7dfa277a120a70fb55f2bd3
shibamoto <silverbirder@gmail.com>
2025-05-30 20:07:29 +0900

    style: format

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -23,5 +23,5 @@ export const posts = createTable(
       .notNull(),
     updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
   }),
-  (t) => [index("name_idx").on(t.name)]
+  (t) => [index("name_idx").on(t.name)],
 );
