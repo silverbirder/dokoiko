7ae1ff749abadf70c347e93c011151657cfb46f0
shibamoto <silverbirder@gmail.com>
2025-05-31 17:04:22 +0900

    feat: add YahooCredit component and export from index

---
+++ src/components/yahoo-credit/yahoo-credit.tsx
@@ -0,0 +1,13 @@
+export function YahooCredit() {
+  return (
+    <span
+      style={{
+        margin: "15px 15px 15px 15px",
+      }}
+    >
+      <a href="https://developer.yahoo.co.jp/sitemap/">
+        Webサービス by Yahoo! JAPAN
+      </a>
+    </span>
+  );
+}
